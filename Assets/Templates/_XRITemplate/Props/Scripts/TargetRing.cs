a5c4c88877147b993c7be9542a57b4ce8d5429b5
Aleksander Sadov <20453629+AleksanderSadov@users.noreply.github.com>
2022-08-24 16:30:55 +0700

    Add OnHitFinished event to ring

--- Assets/Templates/_XRITemplate/Props/Scripts/TargetRing.cs
+++ Assets/Templates/_XRITemplate/Props/Scripts/TargetRing.cs
@@ -6,9 +6,16 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class TargetRing : MonoBehaviour, IAnimationEventActionBegin, IAnimationEventActionFinished
 {
     static readonly string k_ActiveLabel = "active";
+    static readonly string k_hitLabel = "hit";
+
+    [SerializeField]
+    UnityEvent m_OnAwake;
 
     [SerializeField]
     UnityEvent m_OnHit;
+
+    [SerializeField]
+    UnityEvent m_OnHitFinished;
 
     [SerializeField]
     UnityEvent m_OnActive;
@@ -16,20 +23,28 @@ public class TargetRing : MonoBehaviour, IAnimationEventActionBegin, IAnimationE
     [SerializeField]
     UnityEvent m_OnInactive;
 
+    void Awake()
+    {
+        m_OnAwake?.Invoke();
+    }
+
     public void OnHit()
     {
-        m_OnHit.Invoke();
+        m_OnHit?.Invoke();
     }
 
     void IAnimationEventActionBegin.ActionBegin(string label)
     {
         if (label == k_ActiveLabel)
-            m_OnActive.Invoke();
+            m_OnActive?.Invoke();
     }
 
     void IAnimationEventActionFinished.ActionFinished(string label)
     {
         if (label == k_ActiveLabel)
-            m_OnInactive.Invoke();
+            m_OnInactive?.Invoke();
+
+        if (label == k_hitLabel)
+            m_OnHitFinished?.Invoke();
     }
 }
